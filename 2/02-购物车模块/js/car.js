749771f73e826eb14e897238485cd3faadb1396a
xyz <xieyinzhangl@gmail.com>
2022-10-20 11:54:40 +0800

    first commit

--- 2/02-购物车模块/js/car.js
+++ 2/02-购物车模块/js/car.js
@@ -12,5 +12,49 @@ $(function() {
         } else {
             $('.checkall').prop('checked', false);
         }
-    })
+    });
+    // 3.增减商品数量模块,首先声明一个变量,当我们点击+号(increment),就让这个值++,然后赋值给文本框
+    $('.increment').click(function() {
+        var n = $(this).siblings('.itxt').val();
+        n++;
+        $(this).siblings('.itxt').val(n);
+        // console.log(n);
+        // 计算小计模块 
+        var p = $(this).parent().parent().siblings('.p-price').html();
+        p = p.substr(1); // 截取字符串
+        // console.log(p);
+        var price = (p * n).toFixed(2);
+        $(this).parents('.p-num').siblings('.p-sum').html('￥' + price);
+        // var p1 = $('.price-sum').html();
+        // p1 = p1.substr(5);
+        // var sum = p1 + p;
+        // $('.price-sum').text(sum);
+        // console.log(p1, sum);
+    });
+    // 4.减号
+    $('.decrement').click(function() {
+        var n = $(this).siblings('.itxt').val();
+        if (n >= 2) {
+            n--;
+        }
+        $(this).siblings('.itxt').val(n);
+        // console.log(n);
+        // 计算小计模块 
+        var p = $(this).parent().parent().siblings('.p-price').html();
+        p = p.substr(1); // 截取字符串
+        // console.log(p);
+        var price = (p * n).toFixed(2);
+        $(this).parents('.p-num').siblings('.p-sum').html('￥' + price);
+    });
+
+    // 4.用户修改文本框的值 计算小计模块
+    $('.itxt').change(function() {
+        // 先得到文本框里的值 乘以 当前商品的单价
+        var n = $(this).val();
+        // 当前商品的单价
+        var p = $(this).parents('.p-num').siblings('.p-price').html();
+        p = p.substr(1);
+        $(this).parents('.p-num').siblings('.p-sum').html('￥' + (p * n).toFixed(2));
+    });
+
 });
