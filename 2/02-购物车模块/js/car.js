51884eb9179ff99c25ed1760c4306b93a2b7ebe3
xyz <xieyinzhangl@gmail.com>
2022-10-20 20:48:00 +0800

    first commit

--- 2/02-购物车模块/js/car.js
+++ 2/02-购物车模块/js/car.js
@@ -1,9 +1,18 @@
 $(function() {
+
     // 1.全选 全不选功能模块
     $('.checkall').change(function() {
         // console.log($(this).prop('checked'));
         $('.j-checkbox,.checkall').prop('checked', $(this).prop('checked'));
+        if ($(this).prop('checked')) {
+            // 让所有的商品添加 check-cart-item 类名
+            $('.cart-item').addClass('check-cart-item');
+        } else {
+            // 移除类名
+            $('.cart-item').removeClass('check-cart-item');
+        }
     });
+
     // 2.每次点击 小按钮的时候判断被选中的个数是不是等于3,等于3 把全选按钮选上,否则不选
     $('.j-checkbox').change(function() {
         //console.log($('.j-checkbox:checked').length);
@@ -12,7 +21,17 @@ $(function() {
         } else {
             $('.checkall').prop('checked', false);
         }
+
+        if ($(this).prop('checked')) {
+            // 当前商品添加类名
+            $(this).parents('.cart-item').addClass('check-cart-item');
+        } else {
+            // 移除check-cart-item
+            $(this).parents('.cart-item').removeClass('check-cart-item');
+
+        }
     });
+
     // 3.增减商品数量模块,首先声明一个变量,当我们点击+号(increment),就让这个值++,然后赋值给文本框
     $('.increment').click(function() {
         var n = $(this).siblings('.itxt').val();
@@ -27,6 +46,7 @@ $(function() {
         $(this).parents('.p-num').siblings('.p-sum').html('￥' + price);
         getSum();
     });
+
     // 4.减号
     $('.decrement').click(function() {
         var n = $(this).siblings('.itxt').val();
@@ -54,7 +74,9 @@ $(function() {
         $(this).parents('.p-num').siblings('.p-sum').html('￥' + (p * n).toFixed(2));
         getSum();
     });
+
     getSum();
+
     // 5.所有商品总计模块(封装成一个函数)
     function getSum() {
         var count = 0; // 总件数
@@ -71,5 +93,25 @@ $(function() {
 
     }
 
+    // 6.删除商品模块
+    // (1) 商品后面的删除按钮
+    $('.p-action a').click(function() {
+        // 删除的是当前的商品
+        $(this).parents('.cart-item').remove();
+        getSum();
+    });
+
+    // (2)删除选中的商品
+    $('.remove-batch').click(function() {
+        // 删除的是小的复选框选中的商品
+        $('.j-checkbox:checked').parents('.cart-item').remove();
+        getSum();
+    });
+
+    // (3)清空购物车 删除全部商品
+    $('.clear-all').click(function() {
+        $('.cart-item').remove();
+        getSum();
+    })
 
 });
